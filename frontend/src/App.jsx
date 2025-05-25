d81e7708832102dc3e0e58533f48e3a2e84f3d1c
ayushanand89 <ayushanand0108@gmail.com>
2025-05-25 18:15:02 +0530

    add OrderDetailsPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,8 @@ import CollectionPage from "./pages/CollectionPage";
 import ProductDeatails from "./components/Products/ProductDeatails";
 import Checkout from "./components/Cart/Checkout";
 import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
 
 const App = () => {
   return (
@@ -24,6 +26,8 @@ const App = () => {
           <Route path="product/:id" element={<ProductDeatails />} />
           <Route path="checkout" element={<Checkout /> } />
           <Route path="order-confirmation" element={<OrderConfirmationPage /> } />
+          <Route path="order/:id" element={<OrderDetailsPage /> } />
+          <Route path="my-orders" element={<MyOrdersPage /> } />
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
